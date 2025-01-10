485fb8f60947631d7067fc6658eb8dd0b0c110c8
manumafe98 <manumaxerapro@gmail.com>
2025-01-09 23:53:03 -0300

    Improve footer looks

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -3,8 +3,8 @@ import { InstagramIcon, TwitterIcon, FacebookIcon } from "../constants/Icons";
 export const FooterComponent = () => {
   return (
     <footer className="flex justify-center w-full h-16 mt-auto">
-        <div className="flex w-4/5 justify-between content-center border-t-1 border-solid border-main-gray pt-2.5">
-          <p>© {new Date().getFullYear()} Vbnb</p>
+        <div className="flex w-4/5 justify-between items-center border-t-1 border-solid border-main-gray pt-2.5">
+          <p className="font-bold text-gray-500">© {new Date().getFullYear()} Vbnb</p>
           <ul className="flex gap-4">
             <li className="group"><TwitterIcon className="fill-current text-gray-500 group-hover:text-main-orange"/></li>
             <li className="group"><InstagramIcon className="fill-current text-gray-500 group-hover:text-main-orange"/></li>
